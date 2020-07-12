6470016f5836f5fa54060bd82d868381430b8b76
Don Tran <mr.dontran@gmail.com>
2020-07-11 23:28:28 -0400

    added to portfolio, contacts and added welcome modal

---
+++ assets/javascripts/portfolio.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    
+
+    $("#start").on("click",  () => {
+        
+        createSlides()
+
+        
+    });
+
+
+
+    function createSlides (){
+
+
+
+    }
+
+
+
+
+
+
+});
