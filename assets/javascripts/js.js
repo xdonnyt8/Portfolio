5aba03eb0a61b1187b85feb3563aef56a8a3cd4f
Don Tran <mr.dontran@gmail.com>
2020-09-01 21:06:57 -0400

    updated with more projects

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -1,4 +1,38 @@
 $(document).ready(function () {
     
         $('.modal').modal('show');
+
+
+        
+  fetch("https://type.fit/api/quotes")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var randomNum = Math.floor(Math.random() * 1643)
+
+    var quote = (data[randomNum].text)
+    var author = (data[randomNum].author)
+    var authorDiv = $("#authors")
+
+    var quoteDiv = $("#quotes")
+
+    quoteDiv.text(quote);
+    authorDiv.text("-" + author)
+
+    setInterval(function () {
+
+      var randomNum = Math.floor(Math.random() * 1643)
+      var quote = (data[randomNum].text)
+      var author = (data[randomNum].author)
+
+      var authorDiv = $("#authors")
+      var quoteDiv = $("#quotes")
+
+      quoteDiv.text('"' + quote+ '"');
+      authorDiv.text("-" + author);
+    }, 5000);
+
+  });
 });
